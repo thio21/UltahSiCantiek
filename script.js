cb08ecf30148cc6d8fc3c9961ac865e36e32e4d0
thio21 <justitio.fadly@gmail.com>
2024-11-15 19:40:42 +0700

    Ultah

---
+++ script.js
@@ -0,0 +1,13 @@
+function myFunction(){
+  window.open("https://buku-ultah-novi.vercel.app");
+}
+
+function showMessage() {
+  var messageBox = document.getElementById("messageBox");
+  messageBox.style.display = "flex"; // Menampilkan box pesan
+}
+
+function closeMessage() {
+  var messageBox = document.getElementById("messageBox");
+  messageBox.style.display = "none"; // Menyembunyikan box pesan
+}
